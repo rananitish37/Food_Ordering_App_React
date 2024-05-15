dce590adce4929a6d61c0ed77ca91024b5eef37a
Nitish <rananitish37@gmail.com>
2024-05-16 01:39:07 +0530

    starting first project using react

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const heading = <h1>Hello JSX</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
